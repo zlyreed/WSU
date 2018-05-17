4e30a16495066d5749fd0848d5a6a681c7cf6264
zlyreed <6754359+zlyreed@users.noreply.github.com>
2018-05-17 14:39:45 -0400

    Update writeObj_vf.m

--- VHM model/BoneFiles/functions/writeObj_vf.m
+++ VHM model/BoneFiles/functions/writeObj_vf.m
@@ -19,11 +19,11 @@ fid = fopen(newfilename,'w');
 fprintf(fid,'%s\n\n',notes);  % put desired notes in the first line
 
 for nvk=1:1:length(v_data)
-fprintf(fid,'%s %5.5f %5.5f %5.5f\n', 'v', v_data(nvk,1), v_data(nvk,2),v_data(nvk,3));
+fprintf(fid,'%s %.6f %.6f %.6f\n', 'v', v_data(nvk,1), v_data(nvk,2),v_data(nvk,3)); 
 end
 
 for nfk=1:1:length(f_data)
-fprintf(fid,'%s %5.5f %5.5f %5.5f\n', 'f', f_data(nfk,1), f_data(nfk,2),f_data(nfk,3));
+fprintf(fid,'%s %.6f %.6f %.6f\n', 'f', f_data(nfk,1), f_data(nfk,2),f_data(nfk,3));
 end
 
 fclose(fid);
